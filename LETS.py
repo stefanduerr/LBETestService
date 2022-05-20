6861ad512e7991f9d0d4250aff1945225198ca03
stefanduerr <stefan.walter.duerr@gmail.com>
2022-05-20 11:08:51 +0200

    LETS V1

---
+++ LETS.py
@@ -0,0 +1,231 @@
+from dataclasses import asdict
+import json
+from re import A
+import requests
+import random  
+import string
+import matplotlib.pyplot as plt
+import time
+import numpy as np, numpy.random
+import db
+
+c_con, c_curs = db.connect_db()
+headers = {
+  'Accept': 'application/json',
+  'Authorization': 'Token r7drn3dYBB+m4y6GQcVIWv8dO2A0U+0S'
+}
+
+timearr = []
+timearr2 = []
+runs = []
+cou = 0
+
+def measure_time(x):
+  global cou
+  
+  if x:
+    global start
+    start = time.perf_counter()
+  else:
+    cou = cou + 1
+    end = time.perf_counter() - start
+    if (cou % 2) == 0:
+      timearr.append(end * 10000)
+    else:
+      timearr2.append(end * 10000)
+
+def map_serial_to_emp_number():
+  barcode_taken = True
+
+  while barcode_taken:
+    barcode = construct_barcode(8)
+    c_curs.execute("SELECT * FROM barcodes WHERE barcode='{}'".format(barcode))
+    if c_curs.fetchone() is None:
+      c_curs.execute("INSERT INTO barcodes(barcode) VALUES ('{}')".format(barcode))
+      c_con.commit()
+      barcode_taken = False
+
+  card_number = str("['" + input() + "']").lower()
+  print(card_number)
+  counter = 1
+  employeeData = ''
+  while employeeData != '[]':
+    r = requests.get('https://app.planning.nu/lifebrain/api2/employee-data?page={}'.format(counter), headers = headers)
+    employeeData = r.json()["employeeData"]
+
+    # print(counter)
+    for i in employeeData:
+      if str(i['cardNumbers']) == card_number:
+        print(i['firstName'])
+
+        empNr = str(i['partnerLastNamePrefix'])[-4:]
+        SVNr = str(i['partnerLastNamePrefix'])[:-5]
+        dateOfBirth = str(i['partnerLastNamePrefix'])[4:-5]
+
+        json_data = ('''
+        {{"sender_id": "Lifebrain",
+        "firstName": "{}",
+        "lastName": "{}",
+        "email": "{}",
+        "SVNr": "{}",
+        "EmpNr": "{}",
+        "date_of_birth": "{}",
+        "barcode": "{}"}}
+        ''').format(i['firstName'], i['lastName'], i['email'], SVNr, empNr, dateOfBirth, barcode)
+
+    employeeData = str(employeeData)
+    # print(employeeData)
+    if employeeData != '[]':
+      counter += 1
+  return json_data
+
+def printjson():
+  counter = 1
+  employeeData = ''
+  while employeeData != '[]':
+    r = requests.get('https://app.planning.nu/lifebrain/api2/employee-data?page={}'.format(counter), headers = headers)
+
+    employeeData = r.json()["employeeData"]
+    print(employeeData)
+
+    
+    if employeeData != '[]':
+      counter += 1
+
+
+def plot_gaussian(iters):
+  sample_string = 'ABCDEFGHIJKLMNOPQRSTUVWX0123456789'
+  prefix = 'EMPS'
+  arr = []
+  arr2 = []
+  arr3 = []
+  arr4 = []
+
+  for j in range(iters):
+    barcode = prefix + ''.join((random.choice(sample_string)) for x in range(8))
+    val = 0
+    for i in range(len(barcode)):
+      val = val + ord(barcode[i])
+    arr.append(val)
+    arr2.append(barcode)
+
+  for k in range(693,1013):
+    arr = np.array(arr)
+    test = np.where(arr==k)
+    test2 = np.array(test).size
+    arr3.append(k)
+    arr4.append(test2)
+
+  fig = plt.figure()
+  
+  plt.bar(arr3,arr4)
+  plt.xlabel('Quersumme')
+  plt.ylabel('Auftreten')
+  plt.show()
+
+  def minimum():
+    mi = max(arr)
+    lowest = arr2[arr.index(mi)] 
+    sum = 0
+    for k in range(len(arr)):
+      sum = sum + arr[k] 
+    avg = sum / len(arr)    
+    return avg, lowest
+
+def generate_barcode():
+  
+  sample_string = 'ABCDEFGHIJKLMNOPQRSTUVWX0123456789'
+  prefix = 'EMPS'
+  val = 0
+  counter = 0
+
+  while val != 860:
+      counter += 1
+      bar_code = ''
+      bar_code = prefix + ''.join((random.choice(sample_string)) for x in range(8))
+      val = 0
+      for i in range(len(bar_code)):
+        val = val + ord(bar_code[i])
+  
+  return val, bar_code
+
+def construct_barcode(length):
+  
+  map_chars = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X']
+  # length = 8
+  max_val = 33
+  amount_of_v = max_val + 1
+  avg_val_sum = length * (max_val / 2)
+
+  arr = np.random.dirichlet(np.ones(length), size=1)
+  arr = arr.flatten() * avg_val_sum
+  
+  while max(arr) > max_val:
+    for i in range(len(arr)):
+      if arr[i] > max_val:
+        res = (length-1) / length * arr[i]
+        arr[i] = 1 / length * arr[i]
+        for j in range(len(arr)):
+          ap = res / length
+          arr[j] = arr[j] + ap
+
+  arr = np.around(arr)
+
+  if sum(arr) != avg_val_sum:
+    delta = avg_val_sum - sum(arr)
+    for i in range(len(arr)):
+      if delta + arr[i] < amount_of_v:
+        arr[i] = arr[i] + delta 
+        delta = 0
+        
+  string = ''
+  for i in range(len(arr)):
+    string = string + map_chars[int(arr[i])]
+  
+  
+  return 'EMPS' + string
+
+def plot_time(iters):
+  for i in range(iters):
+    (construct_barcode(8))
+    (generate_barcode())
+
+  sr_cons = 0
+  sr_sear = 0
+
+  for i in range(len(timearr)):
+    if timearr[i] < timearr2[i]:
+      sr_sear += 1
+    else:
+      sr_cons += 1
+
+  x = list(range(0, iters))
+  plt.plot(x, timearr, label = "Runtime of searching for barcode")
+  plt.plot(x, timearr2, label = "Runtime of constructing barcode")
+  plt.xlabel('Iterations')
+  plt.ylabel('microseconds')
+  # plt.plot(x, runs, label = 'Number of iterations')
+  plt.suptitle('Runtime comparison, in µs', fontsize=20)
+  plt.title('Constructing barcode was faster {} times. \n Searching for barcode was faster {} times.'.format(sr_cons, sr_sear), fontsize=12)
+  plt.legend()
+  plt.show()
+  
+
+# plot_gaussian(10000000)
+# plot_time(2000)
+
+# GUI, Karte hinhalten? WebApp?
+# print(map_serial_to_emp_number())
+printjson()
+# Send JSON to Orchestra
+
+# wait (async?)
+
+# get Response: Successful Y/N?
+
+# If Yes
+
+# check if barcode unique?
+
+# If Yes
+# Print Barcode
---
+++ app.py
@@ -0,0 +1,136 @@
+from flask import Flask, render_template, flash, request, redirect, url_for, send_from_directory, Response, session
+app = Flask(__name__)
+
+import pyodbc 
+import sys
+import db
+import mysql.connector
+from mysql.connector import Error
+from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user, login_required
+from forms import LoginForm
+from flask_bcrypt import Bcrypt
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
+key = 'AAAACCCC'
+c_con, c_curs = db.connect_db()
+global switch2
+switch2 = False
+
+config = {
+    "SECRET_KEY": "6d8ed540960d1085d183d8e5d236f2da",
+    "TEMPLATES_AUTO_RELOAD": True,
+    "MYSQL_HOST": "10.90.1.110",
+    "MYSQL_USER": "stefan",
+    "MYSQL_PASSWORD": "Start2020!",
+    "MYSQL_DB": "LETS"
+}
+
+mysql = MySQL(app)
+bcrypt = Bcrypt(app)
+app.config.from_mapping(config)
+
+c_curs.execute("SELECT id FROM users WHERE id='{}'".format(1))
+print(c_curs.fetchone(), file=sys.stdout)
+    
+#was ist user id? wie kann ich aus der datenbank user abrufen? SELECT * FROM users WHERE user='admin'
+
+def insert_user(username, password):
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    c_curs.execute("INSERT INTO users(user, password) VALUES ('{}', '{}');".format(username, hashed_password))
+    c_curs.execute("SELECT * FROM users")
+    record = c_curs.fetchall()
+    print(record, file=sys.stdout)
+    c_con.commit()
+
+# insert_user('admin1', 'king')
+
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/home", methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        if request.form.get('action1') == 'VALUE1':
+            
+            
+            print(switch2, file=sys.stderr)
+            print('This is standard output', file=sys.stdout)
+        elif  request.form.get('action2') == 'VALUE2':
+            
+            insert_user("fakeadm", "goodpw")
+            print(switch2, file=sys.stderr)
+            print('This is standard output', file=sys.stdout)
+        else:
+            pass # unknown
+    elif request.method == 'GET':
+        return render_template('home.html', form=request.form)
+    
+    return render_template("home.html")
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+
+        c_curs.execute('SELECT user FROM users WHERE user = "{}"'.format(username))
+        if c_curs.fetchone() is not None:
+            c_curs.execute('SELECT password FROM users WHERE user = "{}"'.format(username))
+            hashed_pw = c_curs.fetchone()[0]
+            print(hashed_pw, file=sys.stdout)
+            pw_check = bcrypt.check_password_hash(hashed_pw, password)
+            print(pw_check, file=sys.stdout)
+
+            c_curs.execute('SELECT id, user FROM users WHERE user = "{}"'.format(username))
+            account = c_curs.fetchone()
+            print(account, file=sys.stdout)
+            if account and pw_check:
+                session['loggedin'] = True
+                session['id'] = account[0]
+                # print(account['id'], file=sys.stdout)
+                # print(account['user'], file=sys.stdout)
+                session['username'] = account[1]
+                print(session, file=sys.stdout)
+                return redirect('/home')
+            else:
+                print(session, file=sys.stdout)
+                msg = 'Incorrect username/password!'
+        else:
+            print(session, file=sys.stdout)
+    return render_template('login.html', title='Login', msg=msg)
+
+@app.route('/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   # Redirect to login page
+   return redirect(url_for('home'))
+
+@app.route("/covid_test")
+def covid_test():
+    if 'loggedin' in session:    
+        return render_template('covid_test.html')
+    else:
+        return redirect(url_for('home'))
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
+
+
+        # {% if current_user.is_authenticated %}
+        # Currently logged in as <b>{{ current_user.username }}</b>
+        # <b><a href="{{ url_for('home') }}">Home</a></b> |
+        # <b><a href="{{ url_for('editscreens') }}">Videos</a></b> |
+        # <b><a href="{{ url_for('logout') }}">Logout</a></b>
+        # {% else %}
+        # <b><a href="{{ url_for('home') }}">Home</a></b> |
+        # <b><a href="{{ url_for('login') }}">Login</a></b> |
+        # <b><a href="{{ url_for('register') }}">Register</a></b>
+        # {% endif %}
+        # </div>
